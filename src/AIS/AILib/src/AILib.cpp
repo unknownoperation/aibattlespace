4ef489738ce35058b4e6647f3229d44f8e909403
unknownoperation <kov30@inbox.ru>
2018-05-10 03:06:39 +0300

    Added PIMPL pattern to AI library *encapsulated private fields by using PIMPL #AIBS-86

--- src/AIS/AILib/src/AILib.cpp
+++ src/AIS/AILib/src/AILib.cpp
@@ -3,40 +3,49 @@
 #include "field_pick.h"
 #include "game_common.h"
 
+struct PIMPL_PLAYER_BASE {
+   std::string playerName;
+   std::string serverAdress;
+   FIELD_BASE *field;
+   GAME_STAGE *stage;
+};
+
+
+
 PLAYER_BASE::PLAYER_BASE()
 {
-   serverAdress = "tcp://127.0.0.1:8800";
-   int port = std::stoi(serverAdress.substr(serverAdress.size() - 4, 4));
-   std::string adress = serverAdress;
+   pimpl->serverAdress = "tcp://127.0.0.1:8800";
+   int port = std::stoi(pimpl->serverAdress.substr(pimpl->serverAdress.size() - 4, 4));
+   std::string adress = pimpl->serverAdress;
    adress = (std::string)adress.substr(0, adress.size() - 4);
    while (!TWO_WAY_CONNECTOR::SetConnection(adress + std::to_string(port), TWO_WAY_CONNECTOR::CONNECTION_TYPE::bind)) {
       ++port;
    }
    TWO_WAY_CONNECTOR::SendData("Initial message");
    Json::Value data  = TWO_WAY_CONNECTOR::ReceiveData();
-   playerName = data["key"].asString();
-   field = new FIELD_MANAGER();
-   field->ParseinitialData(data);
-   stage = new GAME_STAGE();
+   pimpl->playerName = data["key"].asString();
+   pimpl->field = new FIELD_MANAGER();
+   pimpl->field->ParseinitialData(data);
+   pimpl->stage = new GAME_STAGE();
 }
 
 PLAYER_BASE::~PLAYER_BASE()
 {
    TWO_WAY_CONNECTOR::ReleaseConnection();
-   delete field;
-   delete stage;
+   delete pimpl->field;
+   delete pimpl->stage;
 }
 
 FIELD_BASE * PLAYER_BASE::GetField()
 {
-   return field;
+   return pimpl->field;
 }
 
 void PLAYER_BASE::GetData()
 {
    Json::Value msg = TWO_WAY_CONNECTOR::ReceiveData();
-   *stage = ParseGameStage(msg["game_stage"].asString());
-   field->ParseData(msg);
+   *(pimpl->stage) = ParseGameStage(msg["game_stage"].asString());
+   pimpl->field->ParseData(msg);
 }
 
 void PLAYER_BASE::SendData(std::vector<UNIT_RESPONSE> data)
@@ -47,17 +56,17 @@ void PLAYER_BASE::SendData(std::vector<UNIT_RESPONSE> data)
       msg["players"][i]["unitID"] = data[i].id;
       msg["players"][i]["direction"] = GetDirStr(DIRECTION((int)data[i].direction));
    }
-   msg["key"] = playerName;
+   msg["key"] = pimpl->playerName;
    TWO_WAY_CONNECTOR::SendData(msg);
 }
 
 GAME_STAGE PLAYER_BASE::GetGameStage()
 {
-   return *stage;
+   return *(pimpl->stage);
 }
 
 int PLAYER_BASE::GetMyPlayerID()
 {
-   return playerName[0] - '0';
+   return pimpl->playerName[0] - '0';
 }
 
